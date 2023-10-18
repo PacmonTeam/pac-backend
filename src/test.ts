dbce0f8fc7c625d626786f07d618bee2b4ad13f4
Aikdanai Sidhikosol <aikdanai@cleverse.com>
2023-10-18 16:42:26 +0700

    test: reset node in test script

--- src/test.ts
+++ src/test.ts
@@ -1,36 +1,49 @@
 import { ethers } from 'ethers'
 import { readFileSync } from 'fs'
+import yaml from 'js-yaml'
 import path from 'path'
 
-import { compile } from './contracts/compile'
-import { deploy } from './contracts/deploy'
-import { getDefaultSigner } from './contracts/hardhat'
-
-const contractPath = path.resolve(__dirname, '../hardhat/flatten/PacERC20.sol')
-const contractSource = readFileSync(contractPath, 'utf8')
+import { Command, parseCommand } from './lib/commandParser'
+import { compile } from './lib/contracts/compile'
+import { deploy } from './lib/contracts/deploy'
+import { getDefaultSigner, provider } from './lib/contracts/hardhat'
 
 const main = async () => {
-  const compileOutput = await compile({
-    ['PacERC20']: contractSource,
+  const resetResp = await provider.send('hardhat_reset', [])
+  console.log('🚀 turbo ~ file: test.ts:13 ~ resetResp:', resetResp)
+
+  const erc20Source = readFileSync(
+    path.resolve(__dirname, '../hardhat/flatten/PacERC20.sol'),
+    'utf8',
+  )
+  const erc20CompileOutput = await compile({
+    ['PacERC20']: erc20Source,
   })
-  // console.log('🚀 turbo ~ file: test.ts:15 ~ out:', compileOutput)
+
+  const tpacSource = readFileSync(
+    path.resolve(__dirname, '../assets/commands/tpac-deployment.yaml'),
+    'utf8',
+  )
 
   const signer = await getDefaultSigner()
 
-  const deployedContract = await deploy({
-    contractFactory: compileOutput.PacERC20.contractFactory,
-    constructorArguments: [
-      signer.address,
-      ethers.parseUnits('1000000', 18),
-      'Test PAC Token',
-      'tPAC',
-      18n,
-    ],
-  })
-  console.log(
-    '🚀 turbo ~ file: test.ts:29 ~ deployedContract:',
-    deployedContract,
+  let context = {
+    ADMIN: signer.address,
+  }
+
+  const tpacDeployCmd = parseCommand<Command.DeployContract>(
+    tpacSource,
+    context,
   )
+  console.log('🚀 turbo ~ file: test.ts:32 ~ tpacDeployCmd:', tpacDeployCmd)
+
+  const deployedTPAC = await deploy({
+    contractFactory: erc20CompileOutput.PacERC20.contractFactory,
+    constructorArguments: tpacDeployCmd.constructor,
+  })
+  context[tpacDeployCmd.output] = await deployedTPAC.getAddress()
+
+  console.log('🚀 turbo ~ file: test.ts:42 ~ context:', context)
 }
 
 main()
