ce20b58788d2d271b627f68ead0f07f973605774
Aikdanai Sidhikosol <aikdanai@cleverse.com>
2023-10-18 16:42:12 +0700

    feat: parse yaml as deploy config

---
+++ src/lib/commandParser.ts
@@ -0,0 +1,27 @@
+import Handlebars from 'handlebars'
+import YAML from 'yaml'
+
+export namespace Command {
+  export type DeployContractFunction = {
+    name: string
+    arguments: any[]
+  }
+
+  export type DeployContract = {
+    name: string
+    constructor: any[]
+    functions?: DeployContractFunction[]
+    output: string
+  }
+}
+
+export const parseCommand = <T = Command.DeployContract>(
+  commandString: string,
+  context: any,
+): T => {
+  const template = Handlebars.compile(commandString)
+  const commandWithArguments = template(context)
+  return YAML.parse(commandWithArguments, {
+    intAsBigInt: true,
+  })
+}
