5e2dd03256bab0f2f6a62270d77ff0f912e2a9e7
Aikdanai Sidhikosol <null>
2023-10-20 18:15:34 +0700

    feat: rpc url from PUBLIC_RPC_URL

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
 export const PRIVATE_RPC_URL = process.env.PRIVATE_RPC_URL || ''
+export const PUBLIC_RPC_URL = process.env.PUBLIC_RPC_URL || ''
 export const DATABASE_URL = process.env.DATABASE_URL || ''
 export const APP_PORT = process.env.APP_PORT || 3000
