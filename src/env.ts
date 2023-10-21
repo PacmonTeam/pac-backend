f25ce7321a4b5a0ffb0b4719676d9f09063f6e05
chinclubi <c.soonue@gmail.com>
2023-10-21 09:23:03 +0700

    change default port to 3033

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,4 @@ require('dotenv').config()
 
 export const PRIVATE_RPC_URL = process.env.PRIVATE_RPC_URL || ''
 export const DATABASE_URL = process.env.DATABASE_URL || ''
-export const APP_PORT = process.env.APP_PORT || 3000
+export const APP_PORT = process.env.APP_PORT || 3033
