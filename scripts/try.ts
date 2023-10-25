307022bcdc83877cdaf876c2628159e34c4d5e91
Aikdanai Sidhikosol <null>
2023-10-25 13:18:38 +0700

    chore: cleanup deprecated scripts

---
+++ scripts/try.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import _ from 'lodash'
+
+import { PRIVATE_RPC_URL } from '@/env'
+import { EthereumService } from '@/lib/ethereum'
+
+const main = async () => {
+  const resp = await axios.get('https://pacmon.suijin.xyz/api/projects/17')
+  console.log('🚀 turbo ~ file: example.ts:8 ~ resp:', resp.data)
+  const project = resp.data
+  const t = project.templates[0]
+  console.log(t.script)
+
+  const ethereumService = new EthereumService.Service(PRIVATE_RPC_URL)
+  const compileOutput = await ethereumService.compile({
+    ['PacERC20']: t.script,
+  })
+  console.log('🚀 turbo ~ file: example.ts:18 ~ compileOutput:', compileOutput)
+}
+
+main()
